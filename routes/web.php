5d0d6e11813fda199cdaf0a22ab3dd22a51ff9d5
aleksandar <aleksandarustic2@gmail.com>
2018-04-12 19:51:12 +0200

    Section 8 lecture 43 - current

--- routes/web.php
+++ routes/web.php
@@ -10,29 +10,59 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//Route::get('/about', function () {
+//    return 'Hi there this is about page';
+//});
+//
+//Route::get('/contact', 'PostController@contact');
+//
+//Route::get('/post/{id}/{name}', function ($id,$name) {
+//    return 'This is post number'.$id .' With name'.$name;
+//});
+//
+//Route::get('/admin/posts/example',array('as' => 'admin.home',function(){
+//    $route = route('admin.home');
+//    $link = "<a href='".$route."'>Click here</a>";
+//    return $link;
+//}));
+//
+////Route::get('/post/{id}','PostController@index');
+//
+//// Route::resource('posts','PostController');
+//
+//Route::get('/post/{id}/{name}/{password}','PostController@show_post');
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/insert',function (){
+//
+//    DB::insert('INSERT into posts(title,content) VALUES (?,?)',['THIS IS LARAVEL TITLE','THIS IS CONTENT']);
+//
+//});
+//Route::get('/read',function (){
+//
+//    $results = DB::select('SELECT * FROM posts WHERE id = ?',[1]);
+//    $output = '';
+//    foreach ($results as $result){
+//        $output .= $result->title;
+//    }
+//    return $output;
+//});
+
+//Route::get('/update',function(){
+//
+//   $updated =  DB::update('UPDATE posts SET title = "UPDATE TITLE" WHERE id = ?',[1] );
+//
+//   return $updated;
+//});
+
+
+Route::get('/delete',function (){
+
+    $deleted = DB::delete('DELETE FROM posts WHERE id = ?',[1]);
+
+    return $deleted;
 });
-
-Route::get('/about', function () {
-    return 'Hi there this is about page';
-});
-
-Route::get('/contact', 'PostController@contact');
-
-Route::get('/post/{id}/{name}', function ($id,$name) {
-    return 'This is post number'.$id .' With name'.$name;
-});
-
-Route::get('/admin/posts/example',array('as' => 'admin.home',function(){
-    $route = route('admin.home');
-    $link = "<a href='".$route."'>Click here</a>";
-    return $link;
-}));
-
-//Route::get('/post/{id}','PostController@index');
-
-// Route::resource('posts','PostController');
-
-Route::get('/post/{id}/{name}/{password}','PostController@show_post');
